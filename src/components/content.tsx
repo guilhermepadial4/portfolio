2a98b4b89a007fbf9d66da89f68fc3731261fb09
Guilherme Padial <guilherme.padial4@gmail.com>
2025-01-22 15:25:52 -0300

    add: creation experience

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,10 +1,11 @@
 import Avatar from "../assets/Avatar.png";
+import Logos from "../assets/Logos.svg";
 import { Button } from "./ui/button";
 
 export function Content() {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-zinc-900 text-white">
-      <img className="mb-7 h-32 w-32" src={Avatar} alt="avatar" />
+      <img className="mb-7 mt-6 h-32 w-32" src={Avatar} alt="avatar" />
 
       <p className="text-5xl font-extrabold">I do code and</p>
       <p className="text-5xl font-extrabold">make content about it!</p>
@@ -18,7 +19,7 @@ export function Content() {
         </p>
       </div>
 
-      <div className="flex justify-center items-center gap-4 mt-8">
+      <div className="mt-8 flex items-center justify-center gap-4">
         <Button
           variant="outline"
           className="rounded-3xl p-6 text-base text-slate-950"
@@ -26,12 +27,14 @@ export function Content() {
           Get in Touch
         </Button>
 
-        <Button
-          className="rounded-3xl p-6 text-base text-white bg-transparent border-2"
-        >
+        <Button className="rounded-3xl border-2 bg-transparent p-6 text-base text-white">
           Get in Touch
         </Button>
       </div>
+
+      <p className="mt-12 text-xl text-neutral-300">EXPERIENCE WITH</p>
+
+      <img className="mt-10" src={Logos} alt="Logos" />
     </div>
   );
 }
