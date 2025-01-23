7fae4e0f9655aa33ab2c67625c992fc2b68d4012
Guilherme Padial <guilherme.padial4@gmail.com>
2025-01-23 16:00:02 -0300

    add: cards in project

--- src/components/content/projects/card/card.tsx
+++ src/components/content/projects/card/card.tsx
@@ -1,15 +1,28 @@
-function ProjectCard({ image, title, description }) {
+import { ArrowUpRight } from "lucide-react";
+import { Github } from "lucide-react";
+
+export default function ProjectCard({ image, title, description }) {
   return (
-    <div className="max-w-sm overflow-hidden rounded-lg bg-white shadow-lg">
-      <img src={image} alt={title} className="h-48 w-full object-cover" />
+    <div className="relative transform overflow-hidden rounded-lg bg-zinc-800 shadow-lg transition-transform hover:-translate-y-2 hover:shadow-2xl cursor-pointer">
+      <img
+        src={image}
+        alt={title}
+        className="h-48 w-full rounded-t-lg object-cover"
+      />
 
-      <div className="p-4">
-        <h3 className="text-xl font-semibold text-gray-800">{title}</h3>
+      <div className="relative p-4">
+        <div className="flex items-start justify-between">
+          <h3 className="text-xl font-semibold text-white">{title}</h3>
+          <ArrowUpRight className="-mt-2 h-6 w-6 text-slate-600" />
+        </div>
 
-        <p className="mt-2 text-gray-600">{description}</p>
+        <p className="mt-2 text-slate-300">{description}</p>
+
+        <div className="mt-4 flex items-center gap-2 text-slate-400 transition-colors hover:text-white">
+          <Github className="h-5 w-5" />
+          <span >Ver código no GitHub</span>
+        </div>
       </div>
     </div>
   );
 }
-
-export default ProjectCard;
--- src/components/content/projects/project.tsx
+++ src/components/content/projects/project.tsx
@@ -1,5 +1,44 @@
-export default function Projects(){
-    return(
-        <p className="text-4xl text-orange-600 font-extrabold mt-16">Projects</p>
-    )
+import ProjectCard from "./card/card";
+
+const projects = [
+  {
+    image:
+      "https://blog.fellyph.com.br/wp-content/uploads/2016/06/react-js.png",
+    title: "Meu Primeiro Projeto",
+    description:
+      "Este é um projeto incrível que desenvolvi para aprender React.",
+  },
+
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-dboLYv5Ftyde-t6ofPdBz496isYAep_Evw&s",
+    title: "Projeto de Portfólio",
+    description: "Um portfólio com React, Tailwind e ShadCN.",
+  },
+
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-dboLYv5Ftyde-t6ofPdBz496isYAep_Evw&s",
+    title: "Projeto de Portfólio",
+    description: "Um portfólio com React, Tailwind e ShadCN.",
+  },
+];
+
+export default function Projects() {
+  return (
+    <>
+      <p className="mt-16 text-4xl font-extrabold text-orange-600">Projects</p>
+
+      <div className="grid grid-cols-1 gap-6 p-6 md:grid-cols-2 lg:grid-cols-3">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            image={project.image}
+            title={project.title}
+            description={project.description}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
